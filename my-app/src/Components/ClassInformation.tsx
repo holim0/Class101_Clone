e28d3cc1074424a51cca0122c05ae73afd477b47
이희제 <holim1226@gmail.com>
2021-02-23 11:03:05 +0900

    Feat: navbar and class simple info done

--- my-app/src/Components/ClassInformation.tsx
+++ my-app/src/Components/ClassInformation.tsx
@@ -4,6 +4,14 @@ import {
     PurchaseContainer,
     Container,
     ImageContainer,
+    ButtonContainer,
+    Btn,
+    ButtonInside,
+    ClassSimpleInfoContainer,
+    ClassSimpleInfo,
+    Info,
+    ClassMainInfo,
+    ClassSimpleInfoTitle,
 } from "../styles/ClassInfoStyle";
 import mainImg from "../assets/mainImg.png";
 
@@ -12,6 +20,31 @@ const ClassInformation = () => {
         <Container>
             <ClassInfoContainer>
                 <ImageContainer src={mainImg} alt="main"></ImageContainer>
+                <ButtonContainer>
+                    <ButtonInside>
+                        <Btn>후기</Btn>
+                        <Btn>클래스 소개</Btn>
+                        <Btn>커리큘럼</Btn>
+                        <Btn>크리에이터</Btn>
+                        <Btn>커뮤니티</Btn>
+                        <Btn>환불 정책</Btn>
+                        <Btn>추천</Btn>
+                    </ButtonInside>
+                </ButtonContainer>
+                <ClassSimpleInfoContainer>
+                    <ClassSimpleInfoTitle>클래스 정보</ClassSimpleInfoTitle>
+                    <ClassSimpleInfo>
+                        <Info>
+                            클래스 분량 <div>0개 챕터, 0개 세부강의</div>
+                        </Info>
+                        <Info>
+                            수강 가능일 <div>바로 수강 가능</div>
+                        </Info>
+                        <Info>
+                            자막 포함 여부 <div>포함</div>
+                        </Info>
+                    </ClassSimpleInfo>
+                </ClassSimpleInfoContainer>
             </ClassInfoContainer>
             <PurchaseContainer>구매 항목</PurchaseContainer>
         </Container>
--- my-app/src/styles/ClassInfoStyle.ts
+++ my-app/src/styles/ClassInfoStyle.ts
@@ -8,6 +8,7 @@ const Container = styled.div`
 
 const ClassInfoContainer = styled.div`
     width: 100%;
+    position: relative;
     @media only screen and (min-width: 1024px) {
         width: 67%;
         height: auto;
@@ -18,7 +19,90 @@ const PurchaseContainer = styled.div``;
 
 const ImageContainer = styled.img`
     width: 100%;
-    height: 100%;
 `;
 
-export { ClassInfoContainer, PurchaseContainer, Container, ImageContainer };
+const ButtonContainer = styled.div`
+    display: flex;
+    white-space: nowrap;
+    overflow-x: auto;
+    font-size: 14px;
+    width: 100%;
+    justify-content: space-between;
+    margin-top: 20px;
+    box-shadow: rgb(237 239 240) 0px -1px 0px 0px inset;
+`;
+const ButtonInside = styled.div`
+    padding: 1px;
+`;
+
+const Btn = styled.button`
+    color: rgb(133, 138, 141);
+    padding: 14px 0px 13px;
+    outline: none;
+    border: 0;
+    background-color: white;
+    cursor: pointer;
+    margin-right: 24px;
+    &:nth-child(1) {
+        border-bottom: 3px solid black;
+        color: black;
+    }
+    &:focus {
+        border-bottom: 3px solid black;
+        color: black;
+    }
+`;
+
+const ClassSimpleInfoContainer = styled.div`
+    padding-top: 40px;
+    padding-bottom: 40px;
+    border-bottom: 1px solid rgb(237, 239, 240);
+`;
+
+const ClassSimpleInfoTitle = styled.div`
+    font-size: 16px;
+    line-height: 24px;
+    letter-spacing: -0.3px;
+    margin: 0px 0px 16px;
+    font-weight: bold;
+    color: rgb(0, 0, 0);
+`;
+
+const ClassSimpleInfo = styled.div`
+    display: flex;
+    margin: 0;
+    width: 75%;
+    justify-content: space-between;
+`;
+
+const Info = styled.div`
+    font-size: 14px;
+    font-weight: normal;
+    line-height: 20px;
+    letter-spacing: -0.15px;
+    margin: 0px 12px 0px 0px;
+    color: rgb(133, 138, 141);
+    display: flex;
+
+    & > div {
+        color: black;
+        margin-left: 12px;
+    }
+`;
+
+const ClassMainInfo = styled.div``;
+
+export {
+    ClassInfoContainer,
+    PurchaseContainer,
+    Container,
+    ImageContainer,
+    ButtonContainer,
+    Btn,
+    ButtonInside,
+    ClassSimpleInfoContainer,
+    ClassSimpleInfoTitle,
+    ClassSimpleInfo,
+    Info,
+    ClassMainInfo,
+};
