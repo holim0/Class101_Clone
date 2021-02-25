74fe64f75de0c0ad4bc448641e62c36bc6b8ddf1
이희제 <holim1226@gmail.com>
2021-02-25 17:27:32 +0900

    Feat: create redux middleware

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -2,11 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { GlobalStyle } from "./styles/globalStyles";
+import createSagaMiddleware from "redux-saga";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer, { rootSaga } from "./store";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(sagaMiddleware, logger))
+);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
-    <>
+    <Provider store={store}>
         <GlobalStyle />
         <App />
-    </>,
+    </Provider>,
     document.getElementById("root")
 );
